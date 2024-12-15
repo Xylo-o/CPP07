43152db7878663b098ae528dc42f6336b4f66c88
Xyloo <adprzyby@student.42heilbronn.de>
2024-12-15 19:10:21 +0100

    Added files and a template

---
+++ ex01/iter.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 19:06:06 by adprzyby          #+#    #+#             */
+/*   Updated: 2024/12/15 19:08:36 by adprzyby         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+#include <iostream>
+#include "Colors.hpp"
+
+template <typename Iter> void iter(Iter arg1, Iter arg2, Iter arg3) {
+	
+}
